bc04d21e5e58aafb99917624cee3137ea186b518
Harshavarthini Palanivel <harshapalanivel5@gmail.com>
2026-02-16 00:45:03 +0530

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,40 @@
+async function movielist(){
+    let values = document.getElementById("values");
+    let input = document.getElementById("input").value;
+
+    const movie = await fetch(`https://imdb.iamidiotareyoutoo.com/search?q=${input}`);
+    const data = await movie.json();
+
+    app.innerHTML="";
+
+    let movies = data.description[0];
+
+    
+
+        
+    let all = document.createElement("div");
+    all.className ="all"
+
+    let img = document.createElement("img");
+    img.className ="img";
+
+    let name = document.createElement("h2");
+    name.className ="mname";
+
+    let year = document.createElement("h4");
+    year.className = "year";
+        
+    all.appendChild(img);
+    all.appendChild(name);
+    all.appendChild(year);
+
+    values.appendChild(all);
+
+    img.src = movies["#IMG_POSTER"];
+    name.innerText = movies["#TITLE"];
+    year.innerText = movies["#YEAR"];
+
+
+
+
+}
